fe7564e1b2a02faf592661aa90b65a4625153836
在那无风之地 <theratandmice@gmail.com>
2021-05-22 22:01:59 +0800

    Initial Commit

---
+++ 提醒事项/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  提醒事项
+//
+//  Created by 123 on 2021/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
